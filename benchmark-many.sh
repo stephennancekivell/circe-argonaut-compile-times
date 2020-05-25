fac54bd8a349b35e7f0ef75db9a00876f279ab79
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2020-05-25 21:13:24 +0200

    Update versions of Scala, Sbt, and dependencies

--- benchmark-many.sh
+++ benchmark-many.sh
@@ -2,27 +2,27 @@
 
 for i in {1..2}
 do
-	# cd jsoniter-scala
-	# sbt -no-colors clean compile >> ../jsoniter-scala.log
-	# cd ..
-
-	cd circe
-	sbt -no-colors +clean +compile >> ../circe.log
+	cd jsoniter-scala
+	sbt -batch clean +compile >> ../jsoniter-scala.log
 	cd ..
 
-	# cd circe-generic-simple
-	# sbt -no-colors clean compile >> ../circe-generic-simple.log
-	# cd ..
+	cd circe
+	sbt -batch clean +compile >> ../circe.log
+	cd ..
 
-	# cd circe-derivation
-	# sbt -no-colors clean compile >> ../circe-derivation.log
-	# cd ..
+	cd circe-semiauto
+	sbt -batch clean +compile >> ../circe-semiauto.log
+	cd ..
 
-	# cd play-json
-	# sbt -no-colors +clean +compile >> ../play-json.log
-	# cd ..
+	cd circe-derivation
+	sbt -batch clean +compile >> ../circe-derivation.log
+	cd ..
 
-	# cd argonaut
-	# sbt -no-colors +clean +compile >> ../argonaut.log
-	# cd ..
+	cd play-json
+	sbt -batch clean +compile >> ../play-json.log
+	cd ..
+
+	cd argonaut
+	sbt -batch clean +compile >> ../argonaut.log
+	cd ..
 done
